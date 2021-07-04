2ce043d07c69259b545066c24d45a145809064a3
jhons-li <1422146381@qq.com>
2021-07-04 21:30:55 +0800

    增加移动端REM适配

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <template>
   <div id="app">
-<!--  导航链接  -->
-     <h1>H5 头条</h1>
+    <!--  导航链接  -->
+
+    <!--路由出口-->
     <router-view/>
+
+<!--<div>-->
+
+<!--  <van-button type="primary">主要按钮</van-button>-->
+<!--  <van-button type="info">信息按钮</van-button>-->
+<!--  <van-button type="default">默认按钮</van-button>-->
+<!--  <van-button type="warning">警告按钮</van-button>-->
+<!--  <van-button type="danger">危险按钮</van-button>-->
+
+<!--  <van-cell-group>-->
+<!--    <van-cell title="单元格" value="内容" />-->
+<!--    <van-cell title="单元格" value="内容" label="描述信息" />-->
+<!--  </van-cell-group>-->
+
+<!--  &lt;!&ndash;    测试rem 自适应&ndash;&gt;-->
+<!--  <div class="box"> </div>-->
+
+<!--  &lt;!&ndash;项目图标&ndash;&gt;-->
+<!--  <i class="toutiao-h5 toutiao-h5-pinglun"></i>-->
+<!--  <i class="toutiao-h5 toutiao-h5-shoucang"></i>-->
+<!--  <i class="toutiao-h5 toutiao-h5-fenxiang"></i>-->
+
+<!--  &lt;!&ndash;图标&ndash;&gt;-->
+<!--  <div>-->
+<!--    <van-icon name="gift-o" />-->
+<!--    <van-icon name="shop-o" />-->
+<!--    <van-icon name="cart-o"  dot/>-->
+<!--  </div>-->
+
+<!--  <div>-->
+<!--    <van-icon name="shopping-cart" />-->
+<!--    <van-icon name="shopping-cart" dot/>-->
+<!--    <van-icon name="shopping-cart" badge="123"/>-->
+<!--  </div>-->
+
+<!--  <div>-->
+<!--    <van-button icon="star-o" type="primary"/>-->
+<!--    <van-button icon="shopping-cart" type="danger" />-->
+<!--    <van-button icon-prefix="toutiao-h5" icon="pinglun" type="warning"/>-->
+<!--    <van-button icon-prefix="toutiao-h5" icon="yanzhengma" type="default"/>-->
+<!--  </div>-->
+<!--</div>-->
   </div>
+
 </template>
 
 <script>
@@ -14,5 +58,9 @@ export default {
 </script>
 
 <style lang="less">
-
+.box{
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,22 @@ import store from './store'
 // 加載全局樣式
 import './styles/index.less'
 
+// 加载有赞Vant 组件库
+import Vant from 'vant'
+
+// 加载样式
+import 'vant/lib/index.css'
+
+// 自动设置REM 基准值 html标签字体大小
+import 'amfe-flexible'
+
+// 全局注册组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 // 创建VUE根实例  将router store 配置到根实例
-// 把APP 組件 渲染到 #APP
+// 把APP 組件 渲染到 #APP快速上手
 new Vue({
   router,
   store,
