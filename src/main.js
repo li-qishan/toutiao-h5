c4d783964aaa972516ea20687e80a47ec8108fd2
jhons-li <1422146381@qq.com>
2021-07-04 16:26:33 +0800

    項目初始化-調整初始目錄結構

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加載全局樣式
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建VUE根实例  将router store 配置到根实例
+// 把APP 組件 渲染到 #APP
 new Vue({
   router,
   store,
